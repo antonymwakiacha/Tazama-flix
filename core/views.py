abbe37574a6b7d8c27a9ef951e6b478d823854a3
antonymwakiacha <tonywugoma@gmail.com>
2022-04-08 17:42:03 +0300

    created the user profile_create view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+from .models import Profile
 from django.shortcuts import render,redirect
 from django.views import View
 from django.contrib.auth.decorators import login_required
@@ -17,9 +18,21 @@ class ProfileList(View):
         profiles=request.user.profiles.all()
         return render(request,'profileList.html',{'profiles':profiles})
 
-
+@method_decorator(login_required,name='dispatch')
 class ProfileCreate(View):
     def get(self,request,*args,**kwargs):
         #form for creating profile
         form=ProfileForm()
         return render(request,'profileCreate.html',{'form':form})
+
+    def post(self,request,*args,**kwargs):
+        form = ProfileForm(request.POST or None)
+
+        if form.is_valid():
+            #print(form.cleaned_data)
+            profile = Profile.objects.create(**form.cleaned_data)
+            if profile:
+                request.user.profiles.add(profile)
+                return redirect('core:profile_list')
+
+        return render(request, 'profileCreate.html',{'form':form})
