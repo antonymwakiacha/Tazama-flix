83e9a40177c0ac503390338c46cb566ea4753c37
antonymwakiacha <tonywugoma@gmail.com>
2022-04-08 19:48:57 +0300

    added new urls to the watch template

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Home,ProfileList,ProfileCreate
+from .views import Home,ProfileList,ProfileCreate,Watch
 
 app_name = 'core'
 
@@ -7,4 +7,5 @@ urlpatterns=[
     path('',Home.as_view()),
     path('profile/',ProfileList.as_view(),name='profile_list'),
     path('profile/create/', ProfileCreate.as_view(),name='profile_create'),
+    path('watch/<str:profile_id>/',Watch.as_view(),name='watch')
 ]
